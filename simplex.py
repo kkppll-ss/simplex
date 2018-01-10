7e9d14a514e3c0d557ebdec2c37d8dde1eb44caf
zouyao <yao_zou@hotmail.com>
2018-01-10 21:39:05 +0800

    fix data4 and add scipy comparision

--- simplex.py
+++ simplex.py
@@ -1,6 +1,6 @@
 import numpy as np
 from solve_simplex import solve_LP, InfeasibleError, UnboundedError
-
+from scipy.optimize import linprog
 
 def main():
     num_var, num_fun = input().split()
@@ -26,6 +26,26 @@ def main():
     func_constraints = np.array(func_constraints)
     var_constraints = np.array(var_constraints)
 
+    rows, columns = LP_matrix.shape
+
+    c = LP_matrix[0, :columns-1]
+    A_and_b = LP_matrix[1:]
+    A_and_b_ub = A_and_b[func_constraints == -1]
+    A_and_b_lb = A_and_b[func_constraints == 1]
+    A_and_b_lb = -A_and_b_lb
+    A_and_b_ub = np.vstack((A_and_b_ub, A_and_b_lb))
+    A_and_b_eq = A_and_b[func_constraints == 0]
+    A_ub = A_and_b_ub[:, :columns-1]
+    b_ub = A_and_b_ub[:, columns-1]
+    A_eq = A_and_b_eq[:, :columns-1]
+    b_eq = A_and_b_eq[:, columns-1]
+    bounds = [map_var_constraint(v) for v in var_constraints]
+
+
+    result = linprog(c, A_ub, b_ub, A_eq, b_eq, bounds, method='simplex',
+                     options={'bland': True, "maxiter": 100000})
+    print(result)
+
     try:
         solution, solution_value = solve_LP(LP_matrix, var_constraints, func_constraints, verbose=False)
         # +0 is use to format negative 0
@@ -34,7 +54,17 @@ def main():
         print("0")
     except InfeasibleError:
         print("-1")
+    
 
+def map_var_constraint(value):
+    if value == 1:
+        return 0, np.inf
+    elif value == 0:
+        return -np.inf, np.inf
+    elif value == -1:
+        return -np.inf, 0
+    else:
+        raise ValueError('the value {} is not value'.format(value))
 
 if __name__ == '__main__':
     np.set_printoptions(precision=4, suppress=True)
